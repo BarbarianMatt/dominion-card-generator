20ecbef0ac6f3d347b7ab729687eb99a4f77a547
BuildTools <54369561+BarbarianMatt@users.noreply.github.com>
2024-10-18 16:15:33 -0700

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -396,7 +396,7 @@ function initCardImageGenerator() {
             // var yCenterAdjust = bigSymbols > 1 ? 20 : 0;
             // var maxHeightAdjust = bigSymbols > 1 ? 80 : 0;
             var yCenterAdjust = 20;
-            var maxHeightAdjust =75;
+            var maxHeightAdjust =70;
 
             yCenter += yCenterAdjust;
             maxHeight += maxHeightAdjust;
